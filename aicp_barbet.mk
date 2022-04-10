d9f1b6919a0f5a9bd576c268c6e0021ac41bf717
Siluxsept <ronny.horn.rh@gmail.com>
2022-04-10 11:42:32 +0200

    barbet: override product info for safetynet
    Change-Id: I2be5dcef5bb950db3b9653cca8fc6929c1297c4b

--- aicp_barbet.mk
+++ aicp_barbet.mk
@@ -23,8 +23,11 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 12 SP2A.220305.012 8177914 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 12   release-keys"
 
 BUILD_FINGERPRINT := google/barbet/barbet:12/SP2A.220305.012/8177914:user/release-keys
+
+PRODUCT_OVERRIDE_INFO := true
+PRODUCT_OVERRIDE_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
 
 $(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
