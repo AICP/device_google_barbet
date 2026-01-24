600736fe9c5ca75cc5d1eb28c58810351cc5ad54
Siluxsept <ronny.horn.rh@gmail.com>
2026-01-24 12:22:53 +0100

    barbet: AICPify
    Change-Id: Ic3847a7bf46825a6d3637bdbc261e9400e3b7b17

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,8 +17,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_barbet.mk \
     $(LOCAL_DIR)/aosp_barbet_hwasan.mk \
-    $(LOCAL_DIR)/lineage_barbet.mk
+    $(LOCAL_DIR)/aicp_barbet.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_barbet-trunk_staging-userdebug \
-    lineage_barbet-trunk_staging-userdebug
+    aicp_barbet-trunk_staging-userdebug
--- aicp_barbet.mk
+++ aicp_barbet.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/barbet/aosp_barbet.mk)
@@ -16,11 +16,15 @@ include device/google/barbet/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 5a
-PRODUCT_NAME := lineage_barbet
+PRODUCT_NAME := aicp_barbet
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Ron H. (Siluxsept)"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="barbet-user 14 AP2A.240805.005.S4 12281092 release-keys" \
